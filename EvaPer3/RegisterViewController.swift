079ea0ba215a92b306eaaaf0b49dfdde324de806
CiroAntony <85654533+CiroAntony@users.noreply.github.com>
2021-11-28 18:44:23 -0500

    se hizo cambio en el background y se quito una opción innecesaria para la app del conductor.

--- EvaPer3/RegisterViewController.swift
+++ EvaPer3/RegisterViewController.swift
@@ -15,52 +15,30 @@ class RegisterViewController: UIViewController {
     
     @IBOutlet weak var confirmTextField: UITextField!
     
-    @IBOutlet weak var tipoTextField: UITextField!
+   // @IBOutlet weak var tipoTextField: UITextField!
     var selectedTipo: String?
-    var listOfTipos = ["Cliente","Conductor"]
+    var listOfTipos = ["",""]
     
     var db: Firestore!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.crearSelector()
-        self.cerrarSelector()
-        
         
         let settings = FirestoreSettings()
         Firestore.firestore().settings = settings
         db = Firestore.firestore()
     }
-    
-    func crearSelector(){
-        let pickerview = UIPickerView()
-        pickerview.delegate = self
-        pickerview.dataSource = self
-        self.tipoTextField.inputView = pickerview
-    }
-    
-    func cerrarSelector(){
-        let toolBar = UIToolbar()
-        toolBar.sizeToFit()
-        
-        let button = UIBarButtonItem(title: "Seleccionar", style: .plain, target: self, action: #selector(self.dismissAction))
-        toolBar.setItems([button], animated: true)
-        toolBar.isUserInteractionEnabled = true
-        self.tipoTextField.inputAccessoryView = toolBar
-        
-    }
-    
     @objc func dismissAction(){
         self.view.endEditing(true)
     }
     
     @IBAction func createAccount(_ sender: Any) {
         if let nombres = nameTextField.text {
+            if let apellido = lastNameTextField.text{
             if let correo = emailTextField.text {
                 if let contraseña = passwordTextField.text {
                     if let confirmarContraseña = confirmTextField.text {
-                        if let tipoUsuario = tipoTextField.text {
                             
                             if (contraseña != confirmarContraseña) {
                                 let passAlert = UIAlertController(title: "Aceptar", message: "Error al igualar contraseñas", preferredStyle: .alert)
@@ -86,8 +64,9 @@ class RegisterViewController: UIViewController {
             }
         }
     }
-    
 }
+    
+
 
 extension RegisterViewController: UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -97,12 +76,9 @@ extension RegisterViewController: UIPickerViewDelegate, UIPickerViewDataSource, 
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return self.listOfTipos.count
     }
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return self.listOfTipos[row]
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int){
     }
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        self.selectedTipo = self.listOfTipos[row]
-        self.tipoTextField.text = self.selectedTipo
     }
     
 }
