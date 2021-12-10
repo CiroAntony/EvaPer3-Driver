b434a0320e23554659774205f710791bba2cd7ad
Rodolfo Leon <76979731+rodoleon51@users.noreply.github.com>
2021-12-10 14:55:54 -0500

    Update forgotPassViewController.swift

--- EvaPer3/forgotPassViewController.swift
+++ EvaPer3/forgotPassViewController.swift
@@ -7,6 +7,12 @@ class forgotPassViewController: UIViewController {
     
     override func viewDidLoad() {
     super.viewDidLoad()
+         emailTextField.layer.borderWidth = 1.0
+        emailTextField.layer.cornerRadius = 13.0
+        
+        recuperarButton.backgroundColor = UIColor.init(red: 48/255, green: 173/255, blue: 99/255, alpha: 1)
+        recuperarButton.layer.cornerRadius = 25.0
+        recuperarButton.tintColor = UIColor.black
     }
     
     @IBAction func forgotPassButton_Tapped(_ sender: UIButton) {
